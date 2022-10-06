9a9f94ba094e6a75d93721bded97800ab0a52d78
Harsshanth <73587980+Harsshanth@users.noreply.github.com>
2022-10-06 22:37:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+// Creating app
+const app = express();
+
+// GET request for single file
+app.get("/", function (req, res) {
+  res.download("demo.pptx", function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
+// Creating server at port 3000
+app.listen(process.env.PORT || 3000, function (req, res) {
+  console.log("Server started to listen at 3000");
+});
